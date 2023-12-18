eddc4cb6921f81eea2a1d6bb009df389e3546b5b
ilian <iliankukov@gmail.com>
2023-12-18 21:11:25 +0200

    new Commit

--- L9 DOM Intro/07. Shopping-Cart/solution.js
+++ L9 DOM Intro/07. Shopping-Cart/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-   //TODO...
+   const addButtons = Array.from(document.
+      querySelectorAll('button.add-product'));
+      const products = {
+         'Bread': 0.80,
+         'Milk': 1.09,
+         'Tomatoes': 0.99,
+      }
+
+      addButtons.forEach (btn =>{
+      btn.addEventListner()
+      })
 }
